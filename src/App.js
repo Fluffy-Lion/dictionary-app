30cfe2c43ca4c18f4dfa9ba384e7a69bcd9d22cc
Code Nation <codenation@fluffy-lion.lan>
2021-01-08 11:50:58 +0000

    env file test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 
+require('dotenv').config()
+
 function App() {
+
+  const getWordRequest = async () => {
+    const url = ""
+  }
   return (
     <div className="App">
       app.js
