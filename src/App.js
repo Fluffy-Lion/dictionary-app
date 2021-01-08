fd95b4ca518d53faeedfcf7adebf27ea21ef20c4
Code Nation <codenation@Fluffy-Lion.local>
2021-01-08 13:50:48 +0000

    added api cred with word request function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import './App.css';
 
 require('dotenv').config()
@@ -5,10 +6,21 @@ require('dotenv').config()
 function App() {
 
   const getWordRequest = async () => {
-    const url = ""
+    const API = process.env.REACT_APP_API_KEY
+    const url = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=${API}`
+
+    const response = await fetch(url)
+    const responseJson = await response.json()
+
+    console.log(responseJson)
   }
+  useEffect(() => {
+    getWordRequest()
+  }, [])
+  
   return (
     <div className="App">
+    
       app.js
     </div>
   );
